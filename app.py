41f445d56cbcd21f54f1bc2099ddcbef7984c356
Issam Haidaoui <issamhaidaoui@gmail.com>
2025-03-01 13:55:54 +0100

    feat: add AgentRequest model and agent endpoint to handle agent prompts

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import logging
+import os
 
 import controllers
 
@@ -19,6 +20,10 @@ class request(BaseModel):
 class BranchNameRequest(BaseModel):
     context: str
 
+class AgentRequest(BaseModel):
+    prompt: str
+
+VERSION = os.getenv("VERSION")
 
 app = FastAPI()
 
@@ -36,7 +41,11 @@ def generate_branch_name(request: BranchNameRequest, comitId: str | None = None)
     response = controllers.branch_names(request.context, comitId)
     return response
 
+@app.post("/agent")
+def generate_agent_message(agent_request: AgentRequest, comitId: str | None = None):
+    response = controllers.agent(agent_request.prompt, comitId)
+    return response
 
 @app.get("/version")
 def version():
-    return {"version": "0.5"}
+    return {"version": VERSION}
--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,2 +1,3 @@
 from .branch_names import branch_names  # noqa: F401
 from .commit_message import commit_message  # noqa: F401
+from .agent import agent  # noqa: F401
